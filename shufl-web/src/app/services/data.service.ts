596ca5ab2b4aa9cd5b2a3cb8b5c5561a36f0e01d
AdamBOD <adambarryodonovan@gmail.com>
2021-03-18 01:33:57 +0000

    Updated DataService to implement retry logic

--- shufl-web/src/app/services/data.service.ts
+++ shufl-web/src/app/services/data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 
 import { environment } from 'src/environments/environment';
 
@@ -16,16 +16,22 @@ export class DataService {
 
     constructor(private httpClient: HttpClient) {}
 
-    public getAsync<T>(endpoint: string): Promise<T> {
+    public getAsync<T>(endpoint: string, retry: boolean = false): Promise<T> {
         return new Promise((resolve, reject) => {
             let url = `${environment.apiUrl}/${endpoint}`;
+
             this.httpClient.get<T>(url, httpOptions)
                 .subscribe(
                     (data) => {
                         resolve(data as T);
                     },
-                    (err) => {
-                        reject(err);
+                    async (err) => {
+                        if (err instanceof HttpErrorResponse && err.status === 500 && retry === false) {
+                            resolve(await this.getAsync(endpoint, true));
+                        }
+                        else {
+                            reject(err);
+                        }
                     }
                 );
         });
