ca25110ee3d2a866448f1de75469b754df623a7c
AdamBOD <adambarryodonovan@gmail.com>
2021-09-05 22:54:25 +0100

    Added Related Album details

--- shufl-web/src/app/models/download-models/album.model.ts
+++ shufl-web/src/app/models/download-models/album.model.ts
@@ -1,4 +1,5 @@
 import { ArtistDownloadModel } from './artist.model';
+import { GroupAlbumDownloadModel } from "./group-album.model";
 import { ImageDownloadModel } from "./image.model";
 import { TrackDownloadModel } from './track.model';
 
@@ -10,4 +11,5 @@ export class AlbumDownloadModel {
     albumImages!: Array<ImageDownloadModel>;
     artists!: Array<ArtistDownloadModel>;
     tracks!: Array<TrackDownloadModel>;
+    relatedGroupAlbum?: GroupAlbumDownloadModel;
 }
